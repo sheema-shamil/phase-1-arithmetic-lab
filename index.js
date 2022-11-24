5dfd6878bc6b486d70b7bf365de18f30c78f55ae
sheema <sheemashamilfarook@gmail.com>
2022-11-24 14:38:22 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31 
+ let num2 = 2 
+ let multiply = num1*num2 
+  
+ var random = 1 
+  
+ var num3 = 16 
+ var num4 = 6 
+ var mod = num3 % num4 
+  
+ var max = 20
